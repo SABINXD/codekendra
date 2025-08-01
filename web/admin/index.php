0009ade9cd709d75092923175ab7332b12837869
Sabin Dhungana <hwaukff@gmail.com>
2025-08-01 17:31:06 +0545

    admin panel chnage

--- web/admin/index.php
+++ web/admin/index.php
@@ -42,7 +42,7 @@ $admin = getAdmin($_SESSION['admin_auth']);
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="../assets/img/circle-icon.png" alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__shake" src="../assets/img/logo.png" alt="AdminLTELogo" height="60" width="200">
     </div>
 
     <!-- Navbar -->
@@ -97,9 +97,9 @@ $admin = getAdmin($_SESSION['admin_auth']);
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
-        <img src="../assets/img/circle-icon.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">CodeKendra</span>
+      <a href="../index.php" class="brand-link">
+        <img src="../assets/img/logo.png" alt="AdminLTE Logo" height="45" >
+
       </a>
 
       <div class="sidebar">
